64e8ee352422abb6f6ee2e0e39243b2c1513b361
RX_LOST <124406818+RX-LOST@users.noreply.github.com>
2024-04-30 19:39:24 -0700

    Update startup_script.sh

--- startup_script.sh
+++ startup_script.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
+# This is the shell script to start your internet vtx/rx on boot
 
-# go into mjpg folder
+# Go into mjpg folder
 cd /home/pi/mjpg-streamer/mjpg-streamer-experimental/
 
-# Start MJPG-Streamer
-sudo mjpg_streamer -i 'input_uvc.so -d /dev/video0 -r 320x250 -f 15 -n' -o 'output_http.so -w www -p 8080' &
+# Start MJPG-Streamer (Set your resoltion here)
+sudo mjpg_streamer -i 'input_uvc.so -d /dev/video0 -r 1280x720 -f 15 -n' -o 'output_http.so -w www -p 8080' &
 # cd back to home
 cd /home/pi/
-#start pigpiofactory service
+# Start pigpiofactory service
 sudo pigpiod
 # Start WebSocket server
 sudo python3 websocket_server4.py &
