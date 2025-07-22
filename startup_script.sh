dc398051496bcac818bdabafbccc4219ebf7e641
RX_LOST <124406818+RX-LOST@users.noreply.github.com>
2025-07-21 23:57:17 -0700

    Update startup_script.sh
    lowered default resolution to increase fps headroom

--- startup_script.sh
+++ startup_script.sh
@@ -5,7 +5,7 @@
 cd /home/pi/mjpg-streamer/mjpg-streamer-experimental/
 
 # Start MJPG-Streamer (Set your resoltion here)
-sudo mjpg_streamer -i 'input_uvc.so -d /dev/video0 -r 1280x720 -f 15 -n' -o 'output_http.so -w www -p 8080' &
+sudo mjpg_streamer -i 'input_uvc.so -d /dev/video0 -r 720x480 -f 30 -n' -o 'output_http.so -w www -p 8080' &
 # cd back to home
 cd /home/pi/
 # Start pigpiofactory service
